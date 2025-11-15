9d766350d239fc64b037ff7add376badf1ffb263
MicheleTodeschini <micheletodeschini01@gmail.com>
2025-11-15 17:07:24 +0100

    add jumbotron and carousel in the HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
         <Route path='/Prodotti' element={<Prodotti/>}/>
         </Routes>
      </BrowserRouter>
+
     </>
   )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,7 @@
 import { Link } from 'react-router-dom'
+import Magliette from '../img/magliette.jpg'
+import Zaini from '../img/Zaini.jpg'
+import Gioielli from '../img/gioielli.jpg'
 
 
 export default function HomePage() {
@@ -18,6 +21,30 @@ export default function HomePage() {
          <Link className="nav-link" aria-current="page" to="/Prodotti">Prodotti</Link>
      </li>
      </ul>
+        <div className="jumbotron jumbotron-fluid">
+        <div id="carouselExampleFade" className="carousel slide carousel-fade">
+    <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src={Magliette} className="d-block w-100" alt="magliette"/>
+    </div>
+    <div className="carousel-item">
+      <img src={Zaini} className="d-block w-100" alt="zaini"/>
+    </div>
+    <div className="carousel-item">
+      <img src={Gioielli} className="d-block w-100" alt=""/>
+    </div>
+  </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
+        </div>
+
         </>
     )
 
