fb2ba6a326a3f1b375c01cf975780de87ee6d52f
MicheleTodeschini <micheletodeschini01@gmail.com>
2025-11-17 14:12:00 +0100

    add Prodtto page and add link
    Removed map and added button for return to the Prodotti page

--- src/pages/Prodotto.jsx
+++ src/pages/Prodotto.jsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { Link } from 'react-router-dom'
 import { useEffect, useState,  } from "react";
 import { useParams } from "react-router-dom";
 
@@ -19,15 +20,29 @@ export default function Prodotto() {
 
         axios
         .get(urlProdotto)
-        .then(res => setProdotto(res.data))
+        .then(res =>
+            setProdotto(res.data))
+        
            
-    }, []);
+    }, [id]);
 
 
     return(
         <>
         <p>vedo prodotto con id ${prodotto.id}</p>
-        
+            <div>
+                 <div key={prodotto.id} className='col-4 card py-3'>
+                <div className='card-top'>
+                    <img src={prodotto.image} alt={prodotto.title} />
+                </div>
+                <div className='card-bottom'>
+                    <h3>{prodotto.title}</h3>
+                    <p><strong>{prodotto.price}€</strong></p>
+                    <p className='description'>{prodotto.description}</p>
+                    <Link to={`/Prodotti`}>Torna alla lista prodotti</Link>
+                </div>
+            </div>
+            </div>
         </>
     )
 
